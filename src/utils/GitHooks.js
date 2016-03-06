92c2c6a24a3b69285e1559c36deffbd204e3d2e9
skimi <gebski.remi@gmail.com>
2016-03-06 01:34:38 +0100

    feat(alerts): Added success & error ascii alerts

--- src/utils/GitHooks.js
+++ src/utils/GitHooks.js
@@ -1,34 +1,42 @@
 import colors from 'colors'
+import alert from './alert'
 
 export default class GitHooks {
     constructor() {
-        this.hooks = {
-            'applypatch-msg': {},
-            'pre-applypatch': {},
-            'post-applypatch': {},
-            'pre-commit': {},
-            'prepare-commit-msg': {},
-            'commit-msg': {},
-            'post-commit': {},
-            'pre-rebase': {},
-            'post-checkout': {},
-            'post-merge': {},
-            'pre-push': {},
-            'pre-receive': {},
-            update: {},
-            'post-receive': {},
-            'post-update': {},
-            'pre-auto-gc': {},
-            'post-rewrite': {}
-        }
+        this.hooks = {}
+        this.hooksOrder = [
+            'applypatch-msg',
+            'pre-applypatch',
+            'post-applypatch',
+            'pre-commit',
+            'prepare-commit-msg',
+            'commit-msg',
+            'post-commit',
+            'pre-rebase',
+            'post-checkout',
+            'post-merge',
+            'pre-push',
+            'pre-receive',
+            'update',
+            'post-receive',
+            'post-update',
+            'pre-auto-gc',
+            'post-rewrite'
+        ]
     }
 
     register (context, hookName, callback) {
-        if (typeof this.hooks[hookName] === 'undefined') {
+        if (this.hooksOrder.indexOf(hookName) === -1) {
             throw new Error('Wrong hook name')
+        }
+        if (typeof this.hooks[hookName] === 'undefined') {
+            this.hooks[hookName] = {}
         }
         if (typeof this.hooks[hookName][context] === 'undefined') {
             this.hooks[hookName][context] = []
+            if (this.isHookFiredLast(hookName, context)) {
+                this.lastHook = hookName
+            }
         }
         this.hooks[hookName][context].push(callback)
     }
@@ -47,7 +55,24 @@ export default class GitHooks {
             errors.forEach((error) => {
                 console.error(colors.white.bgRed(`${error.type}: ${error.message}`))
             })
+            alert.error()
             process.exit(1)
+        } else if (hookName === this.lastHook) {
+            alert.success()
         }
+    }
+
+    isHookFiredLast (hookName, context) {
+        const contextList = global.context.get()
+
+        if (contextList.indexOf(context) !== -1) {
+            if (typeof this.lastHook === 'undefined') {
+                return true
+            } else if (this.hooksOrder.indexOf(this.lastHook) < this.hooksOrder.indexOf(hookName)) {
+                return true
+            }
+        }
+
+        return false
     }
 }
