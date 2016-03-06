32e9e453a5de5a37b35d749e0478113f095b8ae1
skimi <gebski.remi@gmail.com>
2016-03-06 17:14:29 +0100

    feat(jsConvention): Added jsConvention pre-commit linter check

--- src/components/jsConvention.js
+++ src/components/jsConvention.js
@@ -1,6 +1,27 @@
+import sh from 'shelljs'
+
 export class jsConvention {
     install (dependencies) {
         dependencies.cmd('npm i --save-dev eslint-config-naoned eslint')
+    }
+
+    registerHooks (gitHooks) {
+        gitHooks.add('javascript', 'pre-commit', this)
+    }
+
+    preCommit(errors) {
+        const filesToCheck = global.git.getStagedFiles(/\.js$/)
+        if (!filesToCheck.length) {
+            return errors
+        }
+        if (sh.exec(`node_modules/eslint/bin/eslint.js --color --no-ignore ${filesToCheck.join(' ')}`).code > 0) {
+            errors.push({
+                type: 'javascript',
+                message: 'There are some errors in your js, fix them and try again.'
+            })
+        }
+        global.git.clearTempStaging()
+        return errors
     }
 }
 
--- src/utils/Git.js
+++ src/utils/Git.js
@@ -1,6 +1,9 @@
 import GitHooks from './GitHooks'
 import fs from 'fs'
 import colors from 'colors'
+import sh from 'shelljs'
+
+const STAGING_DIR = '.tmp_staging'
 
 export default class Git {
     constructor (path) {
@@ -27,5 +30,50 @@ export default class Git {
         }
 
         return typeof msg.toString === 'function' && msg.toString().split('\n').shift()
+    }
+
+    getStagedFiles (pattern) {
+        let against = 'HEAD'
+        sh.cd(process.cwd())
+        if (sh.exec('git rev-parse --verify HEAD', { silent: true }).code > 0) {
+            // Initial commit: diff against an empty tree object
+            against = '4b825dc642cb6eb9a060e54bf8d69288fbee4904'
+        }
+
+        // this is the magic:
+        // retrieve all files in staging area that are added, modified or renamed
+        // but no deletions etc
+        let files = sh.exec(`git diff-index --name-only --cached --diff-filter=ACMR ${against} --`, {
+            silent: true
+        }).stdout.trim().split('\n')
+
+        if (!files.length) {
+            console.error(colors.white.bgRed('No files added to stage. Use git add.'))
+            process.exit(1)
+        }
+
+        // Copy contents of staged version of files to temporary staging area
+        // because we only want the staged version that will be commited and not
+        // the version in the working directory
+        this.clearTempStaging()
+        let stagedFiles = []
+        files.forEach((file) => {
+            let id = sh.exec(`git diff-index --cached HEAD ${file} | cut -d " " -f4`, { silent: true }).stdout.trim()
+            let directory = file.replace(/[^\/]+?$/, '')
+            // create staged version of file in temporary staging area with the same
+            // path as the original file so that the phpcs ignore filters can be applied
+            sh.exec(`mkdir -p "${STAGING_DIR}/${directory}"`)
+            sh.exec(`git cat-file blob ${id} > "${STAGING_DIR}/${file}"`)
+            if (typeof pattern  === 'undefined') {
+                stagedFiles.push(`${STAGING_DIR}/${file}`)
+            } else if (pattern.test(file)) {
+                stagedFiles.push(`${STAGING_DIR}/${file}`)
+            }
+        })
+        return stagedFiles
+    }
+
+    clearTempStaging () {
+        sh.exec(`rm -rf ${STAGING_DIR}`)
     }
 }
