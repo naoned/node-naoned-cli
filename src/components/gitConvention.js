74f87942ac34140948d29844e0f57521d2b06719
skimi <gebski.remi@gmail.com>
2016-03-07 21:49:26 +0100

    fix(commit-msg): Fixed npm version commit msg not passing the commit-msg test

--- src/components/gitConvention.js
+++ src/components/gitConvention.js
@@ -23,35 +23,38 @@ export default class gitConvention {
 
     commitMsg (errors) {
         const pattern = /^((fixup! |squash! )?(\w+)(?:\(([^\)\s]+)\))?: (.+))(?:\n|$)/
-        const types = ['feat', 'fix', 'docs', 'style', 'refact', 'perf', 'test', 'misc', 'revert', 'git']
+        const types = ['feat', 'fix', 'docs', 'style', 'refact', 'perf', 'test', 'misc', 'revert']
         const maxLength = 100
         const msg = global.git.getCommitMsg()
         const match = pattern.exec(msg)
+        const npmVersionPattern = /^\d+\.\d+\.\d+$/
 
-        if (!match) {
-            errors.push({
-                type: 'git',
-                message: `"${msg}" does not match "<type>(<scope>): <subject>" !`
-            })
-        } else {
-            const firstLine = match[1]
-            const squashing = Boolean(match[2])
-            const type = match[3]
-            // const scope = match[4]
-            // const subject = match[5]
-
-            if (firstLine.length > maxLength && !squashing) {
+        if (!npmVersionPattern.test(msg)) {
+            if (!match) {
                 errors.push({
                     type: 'git',
-                    message: `Your message is longer than ${maxLength} characters !`
+                    message: `"${msg}" does not match "<type>(<scope>): <subject>" !`
                 })
-            }
+            } else {
+                const firstLine = match[1]
+                const squashing = Boolean(match[2])
+                const type = match[3]
+                // const scope = match[4]
+                // const subject = match[5]
 
-            if (types !== '*' && types.indexOf(type) === -1) {
-                errors.push({
-                    type: 'git',
-                    message: `"${type}" is not allowed type !`
-                })
+                if (firstLine.length > maxLength && !squashing) {
+                    errors.push({
+                        type: 'git',
+                        message: `Your message is longer than ${maxLength} characters !`
+                    })
+                }
+
+                if (types !== '*' && types.indexOf(type) === -1) {
+                    errors.push({
+                        type: 'git',
+                        message: `"${type}" is not allowed type !`
+                    })
+                }
             }
         }
 
